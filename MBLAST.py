598b5bed6e285af6fa397998fe2a345676002abd
MartinxMax <280901844@qq.com>
2023-03-18 16:54:01 +0800

    Djangogo_Sensitive_character_test

--- MBLAST.py
+++ MBLAST.py
@@ -3,6 +3,7 @@
 import textwrap,argparse,sys,requests,threading,random,time
 from urllib.parse import quote
 from concurrent.futures import ThreadPoolExecutor
+from loguru import logger
 version = "@Мартин. Django FUZZ Tool V1.0.0"
 Logo=f'''
 $$\      $$\ $$$$$$$\  $$\        $$$$$$\   $$$$$$\ $$$$$$$$\ 
@@ -15,7 +16,13 @@ $$ | \_/ $$ |$$$$$$$  |$$$$$$$$\ $$ |  $$ |\$$$$$$  |  $$ |
 \__|     \__|\_______/ \________|\__|  \__| \______/   \__|                                                                                                           
                           {version}      
 =============================================================='''
-
+def Init_Loger():
+    logger.remove()  # 清除所有默认处理器
+    logger.add(
+        sink=sys.stdout,
+        format="<green>[{time:HH:mm:ss}]</green><level>[{level}]</level> | <level>{message}</level>",
+        level="INFO"
+    )
 class Main():
     def __init__(self,args):
         self.URL = args.URL
@@ -24,8 +31,7 @@ class Main():
         if self.URL and '*' in self.URL:
             self.get_Page_satat()
         else:
-            print('[ERROR]You did not enter the URL correctly, or did not add * injection points\n[EXIT]')
-
+            logger.error("You did not enter the URL correctly, or did not add * injection points")
 
     def get_Page_satat(self):
         threadPool = ThreadPoolExecutor(max_workers=5, thread_name_prefix="BY_Martin_")
@@ -42,11 +48,11 @@ class Main():
             except:
                 continue
             else:
-                print(f"[INFO][{str(Status.status_code)}]", f"[Lenght]:{len(Status.text)}", f"[PAYLOAD]:{self.URL.replace('*', quote(chr(PAYLOAD), 'utf-8'))}",f"[HEX]:{hex(PAYLOAD)}",f"[Ascii]{PAYLOAD} --['{chr(PAYLOAD)}']")
-
+                logger.info(f"[{str(Status.status_code)}] [Lenght]:{len(Status.text)} [PAYLOAD]:{self.URL.replace('*', quote(chr(PAYLOAD), 'utf-8'))} [HEX]:{hex(PAYLOAD)} [Ascii]{PAYLOAD} --['{chr(PAYLOAD)}']")
 
 def main():
     print(Logo)
+    Init_Loger()
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawTextHelpFormatter,
         epilog=textwrap.dedent('''
